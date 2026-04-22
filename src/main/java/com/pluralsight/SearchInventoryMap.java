8d16836a402c97f7cb6925197336519ea8673240
RickelviC <Cruzrickelvi@gmail.com>
2026-04-22 14:38:04 -0400

    added csv file

--- src/main/java/com/pluralsight/SearchInventoryMap.java
+++ src/main/java/com/pluralsight/SearchInventoryMap.java
@@ -1,4 +1,31 @@
 package com.pluralsight;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class SearchInventoryMap {
+
+    public static HashMap<Integer, Product> inventory =
+            new HashMap<Integer, Product>();
+
+    public static void main(String[] args) {
+        // this method loads product objects into inventory
+        loadInventory();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What item # are you interested in? ");
+        int id = scanner.nextInt();
+        Product matchedProduct = inventory.get(id);
+        if (matchedProduct == null) {
+            System.out.println("We don't carry that product");
+        } else {
+            System.out.printf("We carry %s and the price is $%.2f",
+                    matchedProduct.getName(), matchedProduct.getPrice());
+        }
+    }
+
+    private static void loadInventory(){
+        // Your code should be here
+
+    }
+
 }
